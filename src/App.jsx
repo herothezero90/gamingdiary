bc998b3584393a619ebad54fbea1d5a71a92d32c
herothezero90 <karl.a.lenard@gmail.co>
2024-09-22 11:27:58 +0200

    localstorage useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { ChakraProvider } from '@chakra-ui/react';
 import Login from './components/Login';
 import MainPage from './components/MainPage';
@@ -6,10 +6,25 @@ import MainPage from './components/MainPage';
 function App() {
   const [username, setUsername] = useState('');
 
+  useEffect(() => {
+    const storedUsername = localStorage.getItem('username');
+    if (storedUsername) {
+      setUsername(storedUsername);
+    }
+  }, []);
+
+  useEffect(() => {
+    if (username) {
+      localStorage.setItem('username', username);
+    } else {
+      localStorage.removeItem('username');
+    }
+  }, [username]);
+
   return (
     <ChakraProvider>
       {username ? (
-        <MainPage username={username} />
+        <MainPage username={username} setUsername={setUsername} />
       ) : (
         <Login setUsername={setUsername} />
       )}
